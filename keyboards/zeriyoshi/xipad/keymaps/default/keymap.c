51887f37a35cdd37dcd8382f6cc9789b9b93d0a9
zeriyoshi <zeriyoshi@gmail.com>
2020-07-04 04:30:30 +0900

    xipad: update default layout

--- keyboards/zeriyoshi/xipad/keymaps/default/keymap.c
+++ keyboards/zeriyoshi/xipad/keymaps/default/keymap.c
@@ -17,9 +17,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-                            KC_W,                   KC_O, \
-                    KC_A,   KC_S,   KC_D,   KC_K,   KC_L,   KC_SCLN, \
-        KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_BSPC, \
-                                KC_LCTL,     KC_RCTL
+                                KC_W,                               KC_P, \
+                    KC_A,       KC_S,       KC_D,       KC_L,       KC_SCLN,    KC_QUOT, \
+        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT, \
+                                    KC_V,   KC_M
     )
 };
